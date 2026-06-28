c38f31e95f4f02f69e6e133d86fe800dae4c59ca
Sahariar Hossain <sahariarhossain524@gmail.com>
2026-06-28 12:11:59 +0600

    Fix middleware to make storefront public

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,16 +29,7 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api/auth (NextAuth endpoints)
-     * - api/seed (Seed endpoints)
-     * - login (auth pages)
-     * - register (auth pages)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api/auth|api/seed|login|register|_next/static|_next/image|favicon.ico).*)',
+    '/dashboard/:path*',
+    '/account/:path*'
   ],
 };
